fbd78e72e76b9881cbc80122b172126c4c584465
Vinothini <vinothinidharmaraj@gmail.com>
2018-01-31 19:22:41 -0800

    Adding the scale set vm executer and crawler for virtual machines and scale sets

---
+++ AzureChaos.Core/Entity/VMScaleSetCrawlerResponseEntity.cs
@@ -0,0 +1,15 @@
+﻿using AzureChaos.Entity;
+
+namespace AzureChaos.Entity
+{
+    public class VMScaleSetCrawlerResponseEntity : CrawlerResponseEntity
+    {
+        public VMScaleSetCrawlerResponseEntity(string partitionKey, string rowKey)
+        {
+            this.PartitionKey = partitionKey;
+            this.RowKey = rowKey;
+        }
+
+        public string ResourceName { get; set; }
+    }
+}
---
+++ AzureChaos.Core/Entity/VirtualMachineCrawlerResponseEntity.cs
@@ -0,0 +1,17 @@
+﻿namespace AzureChaos.Entity
+{
+    public class VirtualMachineCrawlerResponseEntity : CrawlerResponseEntity
+    {
+        public VirtualMachineCrawlerResponseEntity(string partitionKey, string rowKey)
+        {
+            this.PartitionKey = partitionKey;
+            this.RowKey = rowKey;
+        }
+        
+        /// <summary>Available Set Id. i.e. Vm belongs to which avaiable set if any.</summary>
+        public string AvailableSetId { get; set; }
+
+        /// <summary>The virtual machine group name i.e. the virtual machine belongs to which resource type ex. is it from Available Set, Scale Set  or Load balancers etc...</summary>
+        public string VirtualMachineGroup { get; set; }
+    }
+}
